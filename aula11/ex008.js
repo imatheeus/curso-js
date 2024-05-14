2b1fed03cf8aabae40ddeec97ec395f6bcb2013e
imatheeus <mathdev00@gmail.com>
2024-05-14 15:47:35 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A veolcidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`DIgira sempre usando o sinto de segurança!`)
